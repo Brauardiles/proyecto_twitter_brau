aa651ea2f83e566a8fee63ba7470db4d71ff9787
Braulio <braulio.esmu@gmail.com>
2020-05-23 12:44:23 -0400

    Hito 2 historia 1 relationship works

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,15 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :retweets, dependent: :destroy
 
-  attribute :image_url
-
+  attribute :image_url, :string, default: 'https://secure.gravatar.com/avatar/8d2ab8f62e9bf17a95aab0dd1c83811e?s=96&d=mm&r=g'
+  # Active
+  has_many :active_relationships, class_name:  "Relationship",
+            foreign_key: "follower_id", dependent: :destroy
+  # Passive
+  has_many :passive_relationships, class_name:  "Relationship",
+            foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Relationship.create(follower_id: 2, followed_id: 5)
