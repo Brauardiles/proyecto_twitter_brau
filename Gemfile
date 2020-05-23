ed8a4ac2cfaedaaebfe35cad81687af7441cd686
Braulio <braulio.esmu@gmail.com>
2020-05-23 15:10:16 -0400

    H2 h2 completed

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'devise'
 
+gem 'activeadmin'
+
 gem 'kaminari'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Relationship.create(follower_id: 2, followed_id: 5)
+
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
